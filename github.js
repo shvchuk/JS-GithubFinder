513ce9e73d0fec45a0e494f99a2f8baa2f2f9d93
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2018-03-19 10:45:09 +0100

    fetching repos

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class GitHub {
     constructor(){
         this.client_id = '14ec488ef2250af25c82';
         this.client_secret = 'efb8e639549aedbd118f4a25314fbbd6eae7ec95';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 
